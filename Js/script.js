bf18d789abf3ebe6da634a268dec962ccce61c7b
Juan8ocho <101667814+Juan8ocho@users.noreply.github.com>
2023-04-24 10:21:12 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,18 @@
+function modonoche() {
+  document.getElementById("all").style.backgroundColor = "black";
+  document.getElementById("all").style.color= 'white';
+  document.getElementById("navbar").style.backgroundColor = "black";
+  document.getElementById("navbar").style.color= 'white';
+  document.getElementById("intro").style.backgroundColor = "black";
+  document.getElementById("intro").style.color= 'white';
+  document.getElementById("titulo").style.backgroundColor = "black";
+  document.getElementById("titulo").style.color= 'white';
+  document.getElementById("job").style.backgroundColor = "black";
+  document.getElementById("job").style.color= 'white';
+  document.getElementById("cartitas1").style.backgroundColor = "black";
+  document.getElementById("cartitas1").style.color= 'white';
+  document.getElementById("cartitas2").style.backgroundColor = "black";
+  document.getElementById("cartiras2").style.color= 'white';
+  document.getElementById("hoby").style.backgroundColor = "black";
+  document.getElementById("hoby").style.color= 'white';
+}
